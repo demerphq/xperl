f729d9e039a6f22cc0f85485c9e7d33af91ec35c
Yves Orton <demerphq@gmail.com>
2026-08-29 03:26:09 +0200

    generator: switch process state by context pointer

--- proto.h
+++ proto.h
@@ -5750,6 +5750,13 @@ Perl_process_state_capture(pTHX_ PERL_PROCESS_STATE *state)
     STMT_START { Perl_assert_aTHX; assert(state); } STMT_END
 
 PERL_CALLCONV void
+Perl_process_state_capture(pTHX_ PERL_PROCESS_STATE *state)
+        Perl_attribute_nonnull_aTHX
+        Perl_attribute_nonnull(pTHX_1);
+#define PERL_ARGS_ASSERT_PROCESS_STATE_CAPTURE  \
+        Perl_assert_aTHX; assert(state)
+
+PERL_CALLCONV void
 Perl_process_state_restore(pTHX_ const PERL_PROCESS_STATE *state)
         Perl_attribute_nonnull_aTHX
         Perl_attribute_nonnull(pTHX_1);
